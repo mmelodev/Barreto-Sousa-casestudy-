54ac562ef879b57f8b76d3b1af0b24fbe9e07101
murilo melo <murilomelomd@gmail.com>
2025-04-23 10:20:48 -0300

    todos os novos commits

--- src/script.js
+++ src/script.js
@@ -5,3 +5,17 @@ togglebtn.addEventListener("click", () => {
   dropdownmenu.classList.toggle("top-[-100%]");
   dropdownmenu.classList.toggle("top-16");
 });
+
+//-------------------
+
+const toggles = document.querySelectorAll(".toggle-btn");
+
+toggles.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const bloco = btn.closest(".bg-[#f5f5f5]");
+    const texto = bloco.querySelector("p");
+
+    const expandido = texto.classList.toggle("line-clamp-3");
+    btn.textContent = expandido ? "Ver mais ↓" : "Ver menos ↑";
+  });
+});
